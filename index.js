b68c7d5698b8b25ccc408b23c6ef04e1d71cec01
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2017-08-11 11:19:52 +0900

    Add vue sample page.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var server = app.listen(3000, function () {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
+app.use(express.static('client'));
+
 app.get("/test", function (req, res, next) {
     res.json({
         "a": 1,
